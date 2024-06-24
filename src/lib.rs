86989caf02bfea358ad6599c5358514547982e9f
TonyWu20 <tony.w21@gmail.com>
2024-06-24 13:32:23 +0800

    Pub re-export

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,26 @@
-#![allow(dead_code)]
+use std::fmt::Display;
 
 use data_dict::DataBlock;
-mod data_dict;
+pub mod data_dict;
 
 pub struct CifFile {
     data_blocks: Vec<DataBlock>,
+}
+
+impl CifFile {
+    pub fn new(data_blocks: Vec<DataBlock>) -> Self {
+        Self { data_blocks }
+    }
+}
+
+impl Display for CifFile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let content = self
+            .data_blocks
+            .iter()
+            .map(|block| format!("{block}"))
+            .collect::<Vec<String>>()
+            .join("\n");
+        write!(f, "{content}")
+    }
 }
