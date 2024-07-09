6de63b3636224caea115f21e102da76fd133a55c
TonyWu20 <tony.w21@gmail.com>
2024-07-09 21:34:44 +0800

    Pub re-export

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
 pub mod data_dict;
 
 mod grammar;
+
+pub use grammar::{
+    CIFDataType, CIFValue, CharString, DataItems, DoubleQuotedString, Float, Integer, LoopColumn,
+    LoopUnit, Number, Numeric, SingleLineData, SingleQuotedString, Tag, TextField, UnquotedString,
+    UnsignedInteger, Value,
+};
+
+#[cfg(feature = "chemrust-core")]
+pub use grammar::chemrust_impl::to_cif_document;
