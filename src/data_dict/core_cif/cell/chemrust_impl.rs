14f26bb6ea6f9054e5e88569db5bdcd36d38a90a
TonyWu20 <tony.w21@gmail.com>
2025-08-27 22:34:49 +0800

    temporary fix: cell angles must be degrees

--- src/data_dict/core_cif/cell/chemrust_impl.rs
+++ src/data_dict/core_cif/cell/chemrust_impl.rs
@@ -9,9 +9,9 @@ pub(crate) fn basic_cell_data<T: UnitCellParameters>(value: &T) -> Vec<DataItems
         CellTerms::Length_a(value.length_a().into()),
         CellTerms::Length_b(value.length_b().into()),
         CellTerms::Length_c(value.length_c().into()),
-        CellTerms::Angle_alpha(value.angle_alpha().into()),
-        CellTerms::Angle_beta(value.angle_beta().into()),
-        CellTerms::Angle_gamma(value.angle_gamma().into()),
+        CellTerms::Angle_alpha(value.angle_alpha().to_degrees().into()),
+        CellTerms::Angle_beta(value.angle_beta().to_degrees().into()),
+        CellTerms::Angle_gamma(value.angle_gamma().to_degrees().into()),
     ];
 
     cell_terms
@@ -25,9 +25,9 @@ pub fn from_unit_cell_parameters(value: &impl UnitCellParameters) -> Vec<DataBlo
         CellTerms::Length_a(value.length_a().into()),
         CellTerms::Length_b(value.length_b().into()),
         CellTerms::Length_c(value.length_c().into()),
-        CellTerms::Angle_alpha(value.angle_alpha().into()),
-        CellTerms::Angle_beta(value.angle_beta().into()),
-        CellTerms::Angle_gamma(value.angle_gamma().into()),
+        CellTerms::Angle_alpha(value.angle_alpha().to_degrees().into()),
+        CellTerms::Angle_beta(value.angle_beta().to_degrees().into()),
+        CellTerms::Angle_gamma(value.angle_gamma().to_degrees().into()),
     ]
     .iter()
     .map(|t| t.to_single_value_data())
